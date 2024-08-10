097c0eb77af11e78f1880eb378963add2b0c08dc
Sameer Choudhary <csameer714@gmail.com>
2024-08-10 13:20:58 +0530

    Adding LoggedInUser data to response

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,19 +1,25 @@
 package com.scm.controllers;
 
-import java.security.Principal;
-
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.scm.entities.User;
 import com.scm.helpers.Helper;
+import com.scm.services.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    private Logger logger = org.slf4j.LoggerFactory.getLogger(UserController.class);
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
 
     // user dashboard page
     @RequestMapping(value = "/dashboard")
@@ -23,11 +29,7 @@ public class UserController {
 
     // user profile page
     @RequestMapping(value = "/profile")
-    public String userProfile(Authentication authentication) {
-        String username = Helper.getEmailOfLoggedInUser(authentication);
-
-        logger.info("User logged in {}", username);
-
+    public String userProfile(Model model, Authentication authentication) {
         return "user/profile";
     }
 
